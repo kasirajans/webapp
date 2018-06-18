789ead90bd01acc465a5ed32713481379a0654e3
kasirajans <kasiece@hotmail.com>
2018-06-18 08:03:00 -0500

    updated moongose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose=require('mongoose');
 const app = express();
+const keys =require('./config/keys')
 
 //Make sure files is excuted 
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 3000;
