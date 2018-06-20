185d7b7ce11e7cde3927628dfcd9e739f5b106fe
kasirajans <kasiece@hotmail.com>
2018-06-20 17:47:59 -0500

    updated prod config files

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,8 @@
 
 module.exports={
 
-    googleClientID:'701280601631-5l8a40m2h316lbk2jjq61ac4qaq0ls6m.apps.googleusercontent.com',
-    googleClientSecret:'a_omb7r7xOeB2W693S9zx-cn',
-    mongoURI: "mongodb://prodadmindb:\"Prod@123\"@ds263710.mlab.com:63710/proddb",
-    cookieKey:'kasitestwebapp'
+    googleClientID:APPSETTING_GOOGLE_CLIENT_ID,
+    googleClientSecret:APPSETTING_GOOGLE_CLIENT_SECRET,
+    mongoURI: APPSETTING_MONGO_URI,
+    cookieKey:APPSETTING_COOKIE_KEY
 };
