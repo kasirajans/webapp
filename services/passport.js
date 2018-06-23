56ad5d61275b8ce14c33bb9a586214d05fec7a13
kasirajans <kasiece@hotmail.com>
2018-06-23 11:13:06 -0500

    updated async fun

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,7 @@ const keys =require('../config/keys');
 const User=mongoose.model('users');
 
 
-// user from returend done fun is passed in this serilaisuser function
+// user from returend google starertgy done fun is passed in this serilaisuser function
 passport.serializeUser((user,done)=>{
 
     // this user.id is returned from mongo DB _ID key value s
@@ -14,6 +14,7 @@ passport.serializeUser((user,done)=>{
 
 });
 
+// WIll contact the mango DB and with the decoded cookie it will search the ID with DB 
 passport.deserializeUser((id,done)=>{
 User.findById(id).then(user=>{
     done(null,user);
@@ -30,27 +31,23 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback',
         //This proxy will trust by google strategy and send redirt URI to https by passport
         proxy:true
-    },(accessToken,refreshToken,profile,done)=>{
+    },async (accessToken,refreshToken,profile,done)=>{
 
-        
-      User.findOne({googleId: profile.id})
-      .then((existingUser)=>{
-          // create promise which will retrun value and stored in a variable "exitingUser"
+        // will return the existing user after a while db finds the user 
+     const existingUser =  await User.findOne({googleId: profile.id})
+      // create promise which will retrun value and stored in a variable "exitingUser"
 if (existingUser) {
     console.log('user already exists '+profile.displayName);
     //user is found without error so giving null and give existinguser this will end the 
     //waiting callback fun
-    done(null, existingUser);
+    return done(null, existingUser);
 }
-else{
+
     console.log('Adding user  '+profile.displayName);
     //create model instance and save it in db
-    new User({googleId: profile.id}).save()
+    const user=await new User({googleId: profile.id}).save()
     //will retrun the user info which is saved in user varibale 
-    .then(user => done(null, user));
-}
-    
-});
+    done(null,user);
 
     }
      
